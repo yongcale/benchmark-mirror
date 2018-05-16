7bbfd5bba3100c69db2fef4fee30fc36a856e2d4
Yong Wu <55wuyong@163.com>
2018-05-15 17:08:56 -0700

    Update setup_tf_hvd.sh

--- test/setup_tf_hvd.sh
+++ test/setup_tf_hvd.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 # 
-# conda install tensorflow and horovod on AWS DLAMI
+# conda install latest tensorflow and horovod on AWS DLAMI
 #
-# usage: sh setup_tf_hvd.sh tfBinaryURL
-#
-# example: sh setup_tf_hvd.sh  \
-#             https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.7.0-cp36-cp36m-linux_x86_64.whl
-
-
-#if [ "$#" -ne 1 ]; then
-#    echo "Invalid number of parameters"
-#fi
+# usage: sh setup_tf_hvd.sh 
 
 # install tensorflow
-pip install --ignore-installed --upgrade tensorflow_gpu #"$1"
-pip3 install --ignore-installed --upgrade tensorflow_gpu
+sudo pip install --ignore-installed --upgrade tensorflow_gpu 
+sudo pip3 install --ignore-installed --upgrade tensorflow_gpu
 
 # install horovod
-#source activate $1; 
-pip install --no-cache-dir horovod
-pip3 install --no-cache-dir horovod
+sudo pip install --no-cache-dir horovod
+sudo pip3 install --no-cache-dir horovod
